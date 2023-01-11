733273b8ed5ff005fac80392404fb4528adaf9bd
mjcommits <mona.jayan@protonmail.com>
2023-01-11 11:56:14 +0530

    first commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.apache.beam.sdk.Pipeline;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Pipeline pipeline = Pipeline.create();
+        System.out.println( "Hello World!" );
+    }
+}
